62e5b0776f22ae06abd9a0c2a9066706329589d4
tedstckd <tedsstacked@gmail.com>
2023-08-01 08:02:42 +0800

    added Update form. added styles for the Update form

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -34,7 +34,34 @@ const Update = () => {
 
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input 
+          type="text" 
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea 
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input 
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Smoothie Recipe</button>
+
+        {/* {formError && <p className="error">{formError}</p>} */}
+      </form>
     </div>
   )
 }
