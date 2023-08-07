8f30c717d9683d3fc2cd06a87a8c5e8ca6d5486d
tedstckd <tedsstacked@gmail.com>
2023-08-07 14:07:19 +0800

    add a delete function

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
 
 const SmoothieCard = ({ smoothie }) => {
+
+    const handleDelete = async () => {
+        const { data, error } = await supabase
+        .from('smoothies')
+        .delete()
+        .eq('id', smoothie.id)
+
+        if (error) {
+            console.log(error)
+        }
+        if (data) {
+            console.log(data)
+        }
+    }
+
     return (
         <div className="smoothie-card">
             <h3>{smoothie.title}</h3>
@@ -9,7 +25,8 @@ const SmoothieCard = ({ smoothie }) => {
             <div className="buttons">  
                 <Link to={'/' + smoothie.id}>
                     <i className="material-icons">edit</i>
-                </Link>    
+                </Link>
+                <i className="material-icons" onClick={handleDelete}>delete</i>
             </div>
         </div>
     )
